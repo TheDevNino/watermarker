3c130eac3565a26136eaa19963608b18d5ce4f48
Nino Cataffo <nino.cataffo@haw-hamburg.de>
2023-12-09 10:57:22 +0100

    Inputs (soweit verfügbar) über Konsole und ausführung über run()

--- main.py
+++ main.py
@@ -19,13 +19,11 @@ class ImageProcessor:
         return alpha
 
     def insert_logo(self, x_start, y_start):
-        x_end = x_start + self.logo_width  # X-Ende des Bildausschnitts berechnen
-        y_end = y_start + self.logo_height  # Y-Ende des Bildausschnitts berechnen
+        x_end = x_start + self.logo_width       # X-Ende des Bildausschnitts berechnen
+        y_end = y_start + self.logo_height      # Y-Ende des Bildausschnitts berechnen
 
         patch = self.main_image[y_start:y_end, x_start:x_end, :]  # Bildausschnitt erhalten
         alpha = self.add_alpha_channel()  # Alphakanal hinzufügen
-
-        # Ausgabe der Dimensionen für Diagnosezwecke
         print("Alphakanal-Größe:", alpha.shape)
         print("Bildausschnitt-Größe:", patch.shape)
 
@@ -55,18 +53,54 @@ def check_patch_dimensions(main_image, logo, x_start, y_start):
         return False  # Dimensionen passen nicht zur Logo-Größe
     return True  # Dimensionen passen zur Logo-Größe
 
+def check_file_path(file_path):
+    try:
+        with open(file_path):
+            pass
+    except FileNotFoundError:
+        return False
+    return True
 
-# Verwendung der Klasse
-processor = ImageProcessor("openCV/opencv_data/board.jpg", "openCV/opencv_data/opencv-logo.png")
-processor.resize_image(1000, 800)
-logo_height, logo_width = processor.get_logo_dimensions()
-x_start = 0
-y_start = 0
+def run():
+    # Eingabe der Dateipfade über die Konsole mit Sicherheitsmechanismen
+    while True:
+        main_image_path = input("Gib den Dateipfad des Hauptbildes ein: ")
+        if not check_file_path(main_image_path):
+            print("Fehler: Dateipfad existiert nicht. Bitte erneut eingeben.")
+        else:
+            break
 
-# Überprüfung der Bildausschnittsdimensionen
-if check_patch_dimensions(processor.main_image, processor.logo, x_start, y_start):
-    processor.insert_logo(x_start, y_start)
-    processor.show_image()
-    print(processor.get_image_sizes())
-else:
-    print("Fehler: Bildausschnittsdimensionen passen nicht zur Logo-Größe.")
+    while True:
+        logo_path = input("Gib den Dateipfad des Logos ein: ")
+        if not check_file_path(logo_path):
+            print("Fehler: Dateipfad existiert nicht. Bitte erneut eingeben.")
+        else:
+            break
+
+    # Verwendung der Klasse
+    processor = ImageProcessor(main_image_path, logo_path)
+    processor.resize_image(1000, 800)
+    logo_height, logo_width = processor.get_logo_dimensions()
+
+    # Eingabe der Bildausschnitts-Koordinaten über die Konsole mit Sicherheitsmechanismen
+    while True:
+        try:
+            x_start = int(input("Gib die X-Startkoordinate für das Logo ein: "))
+            y_start = int(input("Gib die Y-Startkoordinate für das Logo ein: "))
+
+            if not check_patch_dimensions(processor.main_image, processor.logo, x_start, y_start):
+                print("Fehler: Bildausschnittsdimensionen passen nicht zur Logo-Größe.")
+            else:
+                processor.insert_logo(x_start, y_start)
+                processor.show_image()
+                print(processor.get_image_sizes())
+                break
+        except ValueError:
+            print("Fehler: Bitte ganze Zahlen für die Koordinaten eingeben.")
+
+
+if __name__ == '__main__':
+    run()
+
+#   Test Files:
+#   "openCV/opencv_data/board.jpg", "openCV/opencv_data/opencv-logo.png"
